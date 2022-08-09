3cdb48b5faaad7540f1ec6ccfd865b9f414d0123
Joao <jp.bruno.machado@gmail.com>
2022-08-09 16:42:04 -0300

    filtering function done on ListItem component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,28 +5,26 @@ import { useState } from 'react';
 
 
 export default function List() {
-    const [filters, setFilters] = useState(['React', 'Frontend']);
+    const [filters, setFilters] = useState(['JavaScript', 'React']);
 
-    function addFilter(event:any) {
-        const currentItem:string = event.target.dataset.itemname;
+    function addFilter(event: any) {
+        const currentItem: string = event.target.dataset.itemname;
 
         return filters.some(el => el === currentItem) ?
-        () => {return}
-        :
-        setFilters([...filters, currentItem])
+            () => { return }
+            :
+            setFilters([...filters, currentItem])
     }
 
-    function removeFilter(event:any) {
-        const currentItem:string = event.target.dataset.filtername;
-        
+    function removeFilter(event: any) {
+        const currentItem: string = event.target.dataset.filtername;
+
         const newArr = filters.filter((el) => {
             if (el === currentItem) {
                 return false
             }
             return el
         })
-
-        console.log(newArr)
 
         setFilters(newArr)
     }
@@ -36,7 +34,13 @@ export default function List() {
             <Filter filters={filters} removeFilter={removeFilter} />
             <ul className={`list ${filters.length > 0 ? 'filtered' : ''}`}>
                 {
-                    data.map(element => <ListItem addFilter={addFilter} key={Math.random() * 10000} data={element} />)
+                    data.map(element =>
+                        <ListItem
+                            addFilter={addFilter}
+                            filters={filters}
+                            key={Math.random() * 10000}
+                            data={element} />
+                    )
                 }
             </ul>
         </>
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -14,72 +14,89 @@ type ListItemProps = {
         languages: string[],
         tools: string[] | []
     },
-    addFilter: (event:any) => void
+    addFilter: (event: any) => void,
+    filters: string[]
 }
 
-export default function ListItem({ data, addFilter }: ListItemProps) {
+export default function ListItem({ data, addFilter, filters }: ListItemProps) {
+    const allItemFilters = data.languages.concat(data.tools)
+    allItemFilters.push(data.level, data.role)
 
-    return (
-        <>
-            <li className='list_item'>
-                <div className='list_item_content_wrapper'>
-                    <img src={data.logo} height='80px' alt='imagem_vaga'></img>
+    const isFiltered = filters.every(element => {
+        return allItemFilters.find(el => el === element)
+    })
 
-                    <div className='list_item_job_description'>
-                        <span className='list_item_title'>
-                            {data.company}
-                            <span className='list_item_title_options'>
-                                {
-                                    data.new ?
-                                        <div className='list_item_new'><p>NEW!</p></div>
-                                        :
-                                        null
-                                }
-                                {
-                                    data.featured ?
-                                        <div className='list_item_featured'><p>FEATURED</p></div>
-                                        :
-                                        null
-                                }
+    if (isFiltered || filters.length === 0) {
+        return (
+            <>
+                <li className='list_item'>
+                    <div className='list_item_content_wrapper'>
+                        <img src={data.logo} height='80px' alt='imagem_vaga'></img>
+
+                        <div className='list_item_job_description'>
+                            <span className='list_item_title'>
+                                {data.company}
+
+                                <span className='list_item_title_options'>
+                                    {
+                                        data.new ?
+                                            <div className='list_item_new'><p>NEW!</p></div>
+                                            :
+                                            null
+                                    }
+                                    {
+                                        data.featured ?
+                                            <div className='list_item_featured'><p>FEATURED</p></div>
+                                            :
+                                            null
+                                    }
+                                </span>
                             </span>
-                        </span>
-                        <span className='list_item_position'>
-                            {data.position}
-                        </span>
-                        <span className='list_item_more'>
-                            {data.postedAt}
-                            <span className='list_item_dot'></span>
-                            {data.contract}
-                            <span className='list_item_dot'></span>
-                            {data.location}
-                        </span>
-                    </div>
 
-                    <div className='list_item_job_tools'>
-                        <button key={Math.random() * 1000} className='job_tool' onClick={addFilter} data-itemname={data.role}>
-                            {data.role}
-                        </button>
-                        <button key={Math.random() * 1000} className='job_tool' onClick={addFilter} data-itemname={data.level}>
-                            {data.level}
-                        </button>
-                        {
-                            data.tools.length < 1 ?
-                                data.tools.map((element: string) => {
-                                    return (
-                                        <button key={Math.random() * 1000} className='job_tool' onClick={addFilter} data-itemname={element}>
-                                            {element}
-                                        </button>
+                            <span className='list_item_position'>
+                                {data.position}
+                            </span>
+
+                            <span className='list_item_more'>
+                                {data.postedAt}
+                                <span className='list_item_dot'></span>
+                                {data.contract}
+                                <span className='list_item_dot'></span>
+                                {data.location}
+                            </span>
+                        </div>
+
+                        <div className='list_item_job_tools'>
+                            <button key={Math.random() * 1000} className='job_tool' onClick={addFilter} data-itemname={data.role}>
+                                {data.role}
+                            </button>
+                            <button key={Math.random() * 1000} className='job_tool' onClick={addFilter} data-itemname={data.level}>
+                                {data.level}
+                            </button>
+                            {
+                                data.tools.length > 0 ?
+                                    data.tools.map((element: string) => {
+                                        return (
+                                            <button key={Math.random() * 1000} className='job_tool' onClick={addFilter} data-itemname={element}>
+                                                {element}
+                                            </button>
+                                        )
+                                    })
+                                    :
+                                    null
+                            }
+                            {
+                                data.languages
+                                    .map((element: string) =>
+                                        <button key={Math.random() * 1000} className='job_tool' onClick={addFilter} data-itemname={element}>{element}</button>
                                     )
-                                })
-                                :
-                                null
-                        }
-                        {
-                            data.languages.map((element: string) => <button key={Math.random() * 1000} className='job_tool' onClick={addFilter} data-itemname={element}>{element}</button>)
-                        }
+                            }
+                        </div>
                     </div>
-                </div>
-            </li>
-        </>
-    )
+                </li>
+            </>
+        )
+    }
+
+    return null
 }
