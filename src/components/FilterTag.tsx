1886da849f276dc7071201e3eafc0366d5548b45
Joao <jp.bruno.machado@gmail.com>
2022-08-10 14:46:00 -0300

    correções no path das imagens

--- src/components/FilterTag.tsx
+++ src/components/FilterTag.tsx
@@ -7,7 +7,7 @@ export default function FilterTag({ filterName, removeFilter }: TagProps) {
     return (
         <li className='filter_tag'>
             <span>{filterName}</span>
-            <button onClick={removeFilter} data-filtername={filterName} className='filter_tag_button'><img src='/images/icon-remove.svg' alt='remove filter'></img></button>
+            <button onClick={removeFilter} data-filtername={filterName} className='filter_tag_button'><img src='/Joblist/images/icon-remove.svg' alt='remove filter'></img></button>
         </li>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return(
         <header className='header'>
-            <img src='/images/bg-header-desktop.svg' alt='background'></img>
+            <img src='/JobList/images/bg-header-desktop.svg' alt='background'></img>
         </header>
     )
 }
